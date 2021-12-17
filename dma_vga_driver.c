96748e2782885204a052388fc61aaefb5e08de75
ClementRoger <clementrger@gmail.com>
2021-12-17 11:10:25 +0100

    Fusion dma simple et ajout structure et ioctl

--- dma_vga_driver.c
+++ dma_vga_driver.c
@@ -8,19 +8,35 @@
 #include <linux/mm.h>
 #include <linux/slab.h>
 
-
 #define DEVICE_NAME	"dma_vga"
 #define CLASS_NAME	"dma_zynq"
 #define BASE_MINOR	0
 #define MINORS_REQUIRED 1
 
+#define IOC_BIT 12 // bit de l'interruption IOC des registres de contrôle
+
+#define MM2S_CR 0x00 // offset du registre MM1S_DMACR
+#define MM2S_SR 0x04 // offset du registre MM2S_DMASR
+#define MM2S_SA 0x18 // offset du registre MM2S_SA
+#define MM2S_LENGTH 0x28 // offset du registre MM2S_LENGTH
+
+#define S2MM_CR 0x30 // offset du registre S2MM_DMACR
+#define S2MM_SR 0x34 // offset du registre S2MM_DMASR
+#define S2MM_DA 0x48 // offset du registre S2MM_DA
+#define S2MM_LENGTH 0x58// offset du registre S2MM_LENGTH
+
 /* ====== DMA BUFFERS ====== */
 #define TAILLE_BUF_X 640
 #define TAILLE_BUF_Y 480
+#define REGISTER_ADDRESS
+#define BUFFER_ADDRESS
+#define BUFFER_SIZE
 
 uint8_t *buf1;
 uint8_t *buf2;
 
+typedef unsigned int u32;
+typedef u32 size_t;
 
 /* ====== Device ====== */
 static struct class* charClass  = NULL;
@@ -34,7 +50,14 @@ static int dma_vga_mmap(struct file *filp, struct vm_area_struct *vma);
 static int dma_vga_close(struct inode* inode, struct file *filp);
 static long dma_vga_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
 
+extern u32 ioread32(volatile void *addr);
+extern void iowrite32(u32 value, volatile void *addr);
 
+/* ====== Module function definition ====== */
+static int dma_vga_init(void);
+static void dma_vga_exit(void);
+
+/* ====== File operation structure ====== */
 static struct file_operations fops = {
 	.owner = THIS_MODULE,
 	.mmap = dma_vga_mmap,
@@ -42,9 +65,76 @@ static struct file_operations fops = {
 	.release = dma_vga_close
 };
 
-/* ====== Module function definition ====== */
-static int dma_vga_init(void);
-static void dma_vga_exit(void);
+/* ====== DMA STRUCTURES ====== */
+
+struct axi_dma
+{
+  volatile char *register_space; // adresse de base des registres
+  volatile int   rx_done; // booléen pour savor si une lecture est terminée
+  volatile int   tx_done; // booléen pour savoir si une écriture est terminée
+};
+
+struct axi_dma_buffer
+{
+  volatile void *data; // pointeur vers l'adresse de base du buffer
+  size_t size; // taille du buffer
+};
+
+struct axi_dma dma;
+struct axi_dma_buffer dma_buffer;
+
+/* ====== Write function definition ====== */
+void write_dma_simple( struct axi_dma *dma , struct axi_dma_buffer *buffer) {
+
+  dma->tx_done = 0; // on remet le tx_done à 0 pour pouvoir détecter une nouvelle interruption
+  iowrite32(1 | (1 << IOC_BIT), dma->register_space + MM2S_CR); // on active le DMA et l'interruption IOC
+  iowrite32((u32)buffer->data, dma->register_space + MM2S_SA); // on écrit l'adresse du buffer dans SA
+  iowrite32(buffer->size, dma->register_space + MM2S_LENGTH); // on écrit la taille du buffer dans LENGTH
+}
+
+/* ====== Read function definition ====== */
+void read_dma_simple( struct axi_dma *dma , struct axi_dma_buffer *buffer) {
+  
+  dma->rx_done = 0; // on remet le tx_done à 0 pour pouvoir détecter une nouvelle interruption
+  iowrite32(1 | (1 << IOC_BIT), dma->register_space + S2MM_CR); // on active le DMA et l'interruption IOC
+  iowrite32((u32)buffer->data, dma->register_space + S2MM_DA); // on écrit l'adresse du buffer dans DA
+  iowrite32(buffer->size, dma->register_space + S2MM_LENGTH); // on écrit la taille du buffer dans LENGTH
+}
+
+/* ====== Wait transmission function definition ====== */
+void wait_tx_completion(struct axi_dma *dma){
+  while(!dma->tx_done);
+}
+
+/* ====== Wait reception function definition ====== */
+void wait_rx_completion(struct axi_dma *dma)
+{
+  while(!dma->rx_done);
+}
+
+/* ====== Reception IRQ function definition ====== */
+void dma_irq_rx(struct axi_dma *dma)
+{
+  u32 status;
+  status = ioread32(dma->register_space + S2MM_SR); // on lit le registre de status
+  if(status & (1 << IOC_BIT)) // si le bit IOC est à 1 -> une transaction vient de se terminer
+  {
+    iowrite32(status | (1 << IOC_BIT), dma->register_space + S2MM_SR); // on écrit 1 sur le bit IOC du registre SR
+    dma->rx_done = 1; // on met le rx_done à 1 pour débloquer wait_rx_completion
+  }
+}
+
+/* ====== Transmission IRQ function definition ====== */
+void dma_irq_tx(struct axi_dma *dma)
+{
+  u32 status;
+  status = ioread32(dma->register_space + MM2S_SR); // on lit le registre de status
+  if(status & (1 << IOC_BIT)) // si le bit IOC est à 1 -> une transaction vient de se terminer
+  {
+    iowrite32(status | (1 << IOC_BIT), dma->register_space + MM2S_SR); // on écrit 1 sur le bit IOC du registre SR
+    dma->tx_done = 1; // on met le tx_done à 1 pour débloquer wait_tx_completion
+  }
+}
 
 /* ====== File operation function ====== */
 static int dma_vga_mmap(struct file *filp, struct vm_area_struct *vma)
@@ -54,6 +144,18 @@ static int dma_vga_mmap(struct file *filp, struct vm_area_struct *vma)
 
 static long dma_vga_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
+	switch( cmd ){
+
+		case WAIT_TX:
+			wait_tx_completion( dma );
+			break;
+		case START:
+			start_dma();	
+			break;
+		default:
+			pr_err("Unknown command in ioctl syscall\n");	
+	}
+
 	return 0;
 }
 
@@ -62,9 +164,25 @@ static int dma_vga_close(struct inode* inode, struct file *filp)
 	return 0;
 }
 
+static void start_dma(void){
+
+}
+
+static void fill_dma_struct(void){
+
+	dma->register_space = REGISTER_ADDRESS;
+	dma->tx_done = 0;
+	dma->rx_done = 0;
+
+	dma_buffer->data = *BUFFER_ADDRESS;
+	dma_buffer->size = BUFFER_SIZE;
+}
+
 /* ====== Module functions ======*/
 static int dma_vga_init(void)
 {
+	fill_dma_struct();
+
 	if (alloc_chrdev_region(&dev, BASE_MINOR , MINORS_REQUIRED , DEVICE_NAME)) {
 		pr_err("Failed to allocate char dev region\n");
 		goto fail_alloc_chrdev;
